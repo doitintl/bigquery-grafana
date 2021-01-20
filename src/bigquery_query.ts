8d6fded1d797f8c694ec0d042dbe77979145fec5
Tanner Bruce <tanner.bruce@shopify.com>
2021-01-20 17:59:52 -0500

    Check for timeColumn is partitioned column
    When using the same time column as a partitioned column it results in
    having the time column appear twice in the resulting query

--- src/bigquery_query.ts
+++ src/bigquery_query.ts
@@ -390,21 +390,25 @@ export default class BigQueryQuery {
     });
     if (this.target.partitioned) {
       const partitionedField = this.target.partitionedField ? this.target.partitionedField : '_PARTITIONTIME';
-      if (this.templateSrv.timeRange && this.templateSrv.timeRange.from) {
-        const from = `${partitionedField} >= '${BigQueryQuery.formatDateToString(
-          this.templateSrv.timeRange.from._d,
-          '-',
-          true
-        )}'`;
-        conditions.push(from);
-      }
-      if (this.templateSrv.timeRange && this.templateSrv.timeRange.to) {
-        const to = `${partitionedField} < '${BigQueryQuery.formatDateToString(
-          this.templateSrv.timeRange.to._d,
-          '-',
-          true
-        )}'`;
-        conditions.push(to);
+      if (this.target.timeColumn !== partitionedField) {
+        if (this.templateSrv.timeRange && this.templateSrv.timeRange.from) {
+          let fromD = this.templateSrv.timeRange.from;
+          const from = `${partitionedField} >= '${BigQueryQuery.formatDateToString(
+              fromD._d,
+              '-',
+              true
+          )}'`;
+          conditions.push(from);
+        }
+        if (this.templateSrv.timeRange && this.templateSrv.timeRange.to) {
+          let toD = this.templateSrv.timeRange.to;
+          const to = `${partitionedField} < '${BigQueryQuery.formatDateToString(
+              toD._d,
+              '-',
+              true
+          )}'`;
+          conditions.push(to);
+        }
       }
     }
     if (this.target.sharded) {
