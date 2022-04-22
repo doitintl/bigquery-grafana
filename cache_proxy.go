939846ff113530ad247444065584d64f68b8cc5d
ketki-deshpande <100827494+ketki-deshpande@users.noreply.github.com>
2022-04-22 17:31:42 +0530

    Add files via upload
    Added a config file for cache proxy

--- cache_proxy.go
+++ cache_proxy.go
@@ -21,6 +21,7 @@ import (
 	"os"
 	"io"
 	"time"
+	"gopkg.in/yaml.v3"
 )
 
 var client *redis.Client
@@ -28,25 +29,64 @@ var _ http.RoundTripper = &transport{}
 var cache = &sync.Map{}
 var ttl int
 
+type transport struct {
+	http.RoundTripper
+}
+
 type cacheValue struct {
 	httpBody   []byte
 	httpHeader http.Header
 	cacheTime  time.Time
 }
 
+type cacheConfig struct {
+	Bigquery struct {
+		Url string `yaml:"url"`
+	} `yaml:bigquery`
+	Server struct {
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+	} `yaml:"server"`
+}
+
+type redisConfig struct {
+	CacheURL string `json:"url"`
+	CacheDatabase int `json:"database"`
+	CachePassword string `json:"password"`
+}
+
+type requestBody struct {
+	Query string `json:"query"`
+	CacheEnabled bool `json:"cacheEnabled"`
+	Location string `json:"location"`
+	Priority string `json:"priority"`
+	UseLegacySql bool `json:"useLegacySql"`
+	UseQueryCache bool `json:"useQueryCache"`
+	CacheType string `json:"cacheType"`
+	CacheDuration int `json:"cacheDuration"`
+	CacheData redisConfig `json:"cacheData"`
+}
+
 func main() {
 
-	// Reverse proxy
-	remote, err := url.Parse("https://bigquery.googleapis.com")
+	conf, err := readCacheConf("_cache_proxy_config.yml")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	if (len(conf.Server.Host) == 0) || (len(conf.Server.Port) == 0) || (len(conf.Bigquery.Url) == 0) {
+		log.Fatal("No field can be left blank in config file ! Please enter all the required details.")
+	}
+
+	serverUrl := conf.Server.Host + ":" + conf.Server.Port
+
+	remote, err := url.Parse(conf.Bigquery.Url)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// type ErrorObject[]interface{}
-
 	handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
 		return func(w http.ResponseWriter, r *http.Request) {
-			log.Println(r.URL)
 			if (strings.Contains(r.URL.String(), "/redis/validate/")) {
 				redisResp, redisErr := validateRedisConnection(r.URL.String())
 				if redisErr != nil {
@@ -58,6 +98,7 @@ func main() {
 					if err != nil {
 						log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 					}
+
 					w.Write(jsonResp)
 				} else {
 					w.Header().Set("Content-Type", "application/json")
@@ -67,6 +108,7 @@ func main() {
 					if err != nil {
 						log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 					}
+
 					w.Write(jsonResp)
 				}
 			} else {
@@ -78,32 +120,40 @@ func main() {
 	proxy := httputil.NewSingleHostReverseProxy(remote)
 	proxy.Transport = &transport{http.DefaultTransport}
 	http.HandleFunc("/", handler(proxy))
-	err = http.ListenAndServe("127.0.0.1:8880", nil)
+	err = http.ListenAndServe(serverUrl, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-type transport struct {
-	http.RoundTripper
+func readCacheConf(cacheConfigFile string) (*cacheConfig, error) {
+    configFile, err := ioutil.ReadFile(cacheConfigFile)
+    if err != nil {
+        return nil, err
+    }
+
+    config := &cacheConfig{}
+    err = yaml.Unmarshal(configFile, config)
+    if err != nil {
+        return nil, fmt.Errorf("Error in file %q: %v", cacheConfigFile, err)
+    }
+
+    return config, nil
 }
 
 // function pings the redis server and returns the response. 
 // Invoked while saving the plugin at datasource panel in grafana.
 func validateRedisConnection(redisCacheUrl string) (resp string, err error) {
 	splitURL := strings.Split(redisCacheUrl, "/redis/validate/")[1]
-	log.Println("spliturl: ",splitURL)
-
 	redisParams := strings.Split(splitURL, "/")
-	
 	redisUrl,_ := url.PathUnescape(redisParams[0])
 	redisUrl,_ = url.PathUnescape(redisUrl)
-	log.Println("redisUrl: ", redisUrl)
 
 	redisDb,_convErr := strconv.Atoi(redisParams[1])
 	if _convErr != nil {
 		return "", errors.New("Invalid Database")
 	}
+
 	redisPassword := ""
 	if len(redisParams) > 2 {
 		redisPassword = redisParams[2]
@@ -120,43 +170,48 @@ func validateRedisConnection(redisCacheUrl string) (resp string, err error) {
 	if redisErr != nil {
 		return "", redisErr
 	}
+
 	return redisResp, nil
 }
 
 func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
 	var key string
 	request,error:=requestJson(req) 
-	log.Printf("duration: %v \n", request.CacheDuration)
+	// log.Printf("duration: %v \n", request.CacheDuration)
 
 	// check if cache is enabled
 	if error!=nil{
 		log.Printf("%v", request.CacheEnabled)
 	}
+
 	q:=request.Query
 	if !request.CacheEnabled {
 		resp, err = t.RoundTripper.RoundTrip(req)
 		if err != nil {
 			return nil, err
 		}
+
 		return resp, nil
 	} else {
 		if q != "" {
 			x := sha256.Sum256([]byte(q))
 			key = hex.EncodeToString(x[:])
-			log.Printf("key: %x \n", key)
+			// log.Printf("key: %x \n", key)
 		}
+
 		if key != "" {
 			// if cache type is 'Redis'
 			if request.CacheType=="redis"{
 				ctx := context.Background()
 				// Redis url from input data
 				redisURL := request.CacheData.CacheURL
-				log.Printf("redisURL : %v\n", redisURL)
+				// log.Printf("redisURL : %v\n", redisURL)
 				
 				if strings.Contains(redisURL, "://") {
 					addr :=strings.Split(redisURL, "/")
 					redisURL = addr[2]
 				}
+
 				// create a redis client and connect to server
 				client = redis.NewClient(&redis.Options{
 					Addr:   	redisURL ,
@@ -169,6 +224,7 @@ func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error
 					log.Fatalf("unable to connect to redis %v, %v\n", redisURL, err)
 					return nil, err
 				}
+
 				log.Println("successfully connected to redis")
 				// if query data is present in cache, return from cache
 				if b, err := client.Get(req.Context(), key).Result(); err == nil && b != "" {
@@ -177,11 +233,11 @@ func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error
 					}
 					resp.Body = ioutil.NopCloser(bytes.NewReader([]byte(b)))
 					resp.ContentLength = int64(len(b))
-					//resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
 					resp.Header.Set("Content-Encoding", "gzip")
 					resp.StatusCode = http.StatusOK;
 					log.Println("returning resp from cache!!")
 					return resp, nil
+
 				} else {
 					// if query data is not preset in cache, connect to bigquery, save data to redis cache and return results to grafana 
 					resp, err = t.RoundTripper.RoundTrip(req)
@@ -194,25 +250,26 @@ func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error
 						if err != nil {
 								return nil, fmt.Errorf("error reading remote response body: %v", err)
 						}
+
 						if err := resp.Body.Close(); err != nil {
 								return nil, fmt.Errorf("error closing remote response body: %v", err)
 						}
+
 						fmt.Println("storing cache entry")
 						b = bytes.Replace(b, []byte("server"), []byte("schmerver"), -1)
 						resp.Body = ioutil.NopCloser(bytes.NewReader(b))
 						resp.ContentLength = int64(len(b))
 						resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
 						if key != "" {
-							if int(request.CacheDuration) > 0 {				// <--
+							if int(request.CacheDuration) > 0 {				
 								fmt.Printf("using cache duration: %v \n", request.CacheDuration)
 								client.Set(req.Context(), key, string(b), time.Duration(int(request.CacheDuration)) * time.Minute) 
 							} else {
 								log.Printf("Invalid Cache Duration in the input !")
-								// client.Set(req.Context(), key, string(b), time.Duration(24) * time.Hour)  
-								// client.Set(req.Context(), key, string(b), 0) <--
 							}
 							
 						}
+
 						log.Println("returning resp from bigquery!!")
 						return resp, nil
 					}
@@ -225,34 +282,38 @@ func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error
 					fmt.Println("Not a post method, proceed as-is")
 					return t.RoundTripper.RoundTrip(req)
 				}
+
 				var body map[string]interface{}
 				reqBytes, err := ioutil.ReadAll(req.Body)
 				if err != nil {
 						return nil, fmt.Errorf("error reading request body")
 				}
+
 				if err := json.Unmarshal(reqBytes, &body); err != nil {
-						panic(err)
+						return nil, fmt.Errorf("error reading request body")		
 				}
+
 				query, ok := body["query"].(string)
 				if !ok {
 						// No query object to worry about caching with, just proxy as-is.
 						fmt.Println("no body object, proceed as-is")
 						return t.RoundTripper.RoundTrip(req)
 				}
+
 				hash := md5.Sum([]byte(query))
 				cacheKey := hex.EncodeToString(hash[:])
 				v, found := cache.Load(cacheKey)
 				fmt.Fprintf(os.Stdout, "key: %v, cache-hit: %v \n", cacheKey, found)
 				if found {
 						cachedValue := v.(*cacheValue)
-						if int(request.CacheDuration) > 0 {					// <---- new
+						if int(request.CacheDuration) > 0 {					
 							ttl = int(request.CacheDuration) * 60
 						} else {
 							log.Printf("Invalid Cache Duration in the input !")
 							// ttl = 86400
 						}
-						fmt.Println("ttl: ", ttl)
-						if int(time.Now().Sub(cachedValue.cacheTime).Seconds()) < ttl{ 	// <--------change
+
+						if int(time.Now().Sub(cachedValue.cacheTime).Seconds()) < ttl{ 	
 							fmt.Println("returning response from cache")
 							b := bytes.NewBuffer(cachedValue.httpBody)
 							resp = &http.Response{
@@ -277,14 +338,17 @@ func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error
 				if err != nil {
 						return nil, err
 				}
+
 				if resp.StatusCode == http.StatusOK {
 						b, err := ioutil.ReadAll(resp.Body)
 						if err != nil {
 								return nil, fmt.Errorf("error reading remote response body: %v", err)
 						}
+						
 						if err := resp.Body.Close(); err != nil {
 								return nil, fmt.Errorf("error closing remote response body: %v", err)
 						}
+
 						fmt.Println("storing cache entry")
 						v := &cacheValue{httpBody: b, httpHeader: resp.Header, cacheTime: time.Now()}
 						cache.Store(cacheKey, v)
@@ -304,9 +368,11 @@ func query(r *http.Request) string {
 	}
 
 	var b bytes.Buffer
+
 	if _, err := b.ReadFrom(r.Body); err != nil {
 		return ""
 	}
+
 	r.Body = ioutil.NopCloser(&b)
 	body := ioutil.NopCloser(bytes.NewReader(b.Bytes()))
 	decoder := json.NewDecoder(body)
@@ -316,36 +382,20 @@ func query(r *http.Request) string {
 		log.Printf("error decoding query, %v\n", err1)
 		return ""
 	}
+
 	log.Printf("query : %v\n", q.Query)
 	return q.Query
-}
-
-type redisConfig struct {
-	CacheURL string `json:"url"`
-	CacheDatabase int `json:"database"`
-	// CacheUsername string `json:"username"`
-	CachePassword string `json:"password"`
-}
-
-type requestBody struct {
-	Query string `json:"query"`
-	CacheEnabled bool `json:"cacheEnabled"`
-	Location string `json:"location"`
-	Priority string `json:"priority"`
-	UseLegacySql bool `json:"useLegacySql"`
-	UseQueryCache bool `json:"useQueryCache"`
-	CacheType string `json:"cacheType"`
-	CacheDuration int `json:"cacheDuration"`
-	CacheData redisConfig `json:"cacheData"`
 }
 
 func requestJson(r *http.Request) (requestBody,error) {
 	var q requestBody
 	var b bytes.Buffer
+
 	if r.Body != nil{
 		if _, err := b.ReadFrom(r.Body); err != nil {
 			return q, err
 		}
+
 		r.Body = ioutil.NopCloser(&b)
 		body := ioutil.NopCloser(bytes.NewReader(b.Bytes()))
 		decoder := json.NewDecoder(body)
@@ -355,9 +405,10 @@ func requestJson(r *http.Request) (requestBody,error) {
 			log.Printf("error decoding query, %v\n", err1)
 			return q, err1
 		}
-		log.Printf("query : %v\n", q.Query)
-		log.Printf("cacheEnabled : %v\n", q.CacheEnabled)
-		log.Printf("cacheType : %v\n", q.CacheType)
+
+		// log.Printf("query : %v\n", q.Query)
+		// log.Printf("cacheEnabled : %v\n", q.CacheEnabled)
+		// log.Printf("cacheType : %v\n", q.CacheType)
 		return q, nil
 	} else {
 		return q, nil
